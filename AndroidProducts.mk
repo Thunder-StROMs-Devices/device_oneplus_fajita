ac751aecd534348ba359265dfbc248090cfb721f
SevralT <alex_chycherin@outlook.com>
2021-06-04 11:37:02 +0300

    fajita: Thunder-StROMs Bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fajita.mk
+    $(LOCAL_DIR)/thunder_fajita.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_fajita-user \
-    lineage_fajita-userdebug \
-    lineage_fajita-eng
+    thunder_fajita-user \
+    thunder_fajita-userdebug \
+    thunder_fajita-eng
